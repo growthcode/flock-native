9696fdb5c2b79d683b7e2ffe9fee1e2d64e8be04
growthcode <growthcode@gmail.com>
2017-11-10 07:47:40 -0800

    split activities into container and component

---
+++ app/components/activities/index.js
@@ -0,0 +1,44 @@
+import React, { PropTypes } from 'react'
+import { View, Button, StyleSheet, Text } from 'react-native'
+
+// Activities.propTypes = {
+//   navigation: PropTypes.shape({
+//     state: PropTypes.func.isRequired,
+//   }).isRequired,
+// };
+
+export default function Activities (props) {
+  return (
+    <View>
+      <View>
+        <Text>
+          Activities
+        </Text>
+      </View>
+
+      <View>
+        <Button
+          onPress={() => props.navigate('DrawerToggle')}
+          title='DrawerToggle' />
+      </View>
+
+      <View>
+        <Button
+          onPress={() => props.navigate('TempOther')}
+          active={ props.routeName === 'TempOther' }
+          title='TempOther' />
+      </View>
+
+      <View>
+        <Button
+          onPress={() => props.navigate('Activities')}
+          active={ props.routeName === 'Activities' }
+          title='Activities' />
+      </View>
+    </View>
+  )
+}
+
+const styles = StyleSheet.create({
+
+})
--- app/containers/activities/index.js
+++ app/containers/activities/index.js
@@ -1,46 +1,16 @@
-import React, { PropTypes, Component } from 'react'
-import { View, Button, Text } from 'react-native'
+import React, { Component } from 'react'
+import Activities from '~/components/activities'
 
-export default class Activities extends Component {
+export default class ActivitiesContainer extends Component {
   static propTypes = {}
-
   render () {
     const { navigate } = this.props.navigation
     const { routeName } = this.props.navigation.state
     return (
-      <View>
-        <View>
-          <Text>
-            Activities
-          </Text>
-        </View>
-
-        <View>
-          <Button
-            onPress={() => navigate('DrawerToggle')}
-            title='DrawerToggle' />
-        </View>
-
-        <View>
-          <Button
-            onPress={() => navigate('TempOther')}
-            active={routeName === 'TempOther'}
-            title='TempOther' />
-        </View>
-
-        <View>
-          <Button
-            onPress={() => navigate('Activities')}
-            active={routeName === 'Activities'}
-            title='Activities' />
-        </View>
-      </View>
+      <Activities
+        navigate={ navigate }
+        routeName={ routeName }
+      />
     )
   }
 }
-
-// Activities.propTypes = {
-//   navigation: PropTypes.shape({
-//     state: PropTypes.func.isRequired,
-//   }).isRequired,
-// };
